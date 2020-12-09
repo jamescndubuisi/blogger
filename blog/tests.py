744ca93df53af19287790d6218d7f79ea5d04a7d
root <jamescndubuisi@yahoo.com>
2020-12-09 11:43:42 +0100

    url tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,100 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+from .views import (
+api_home,
+ArticleListView,
+article_create,
+UpdateArticle,
+ArticleDetail,
+article_delete,
+CommentEditDeleteView,
+CommentViewSet,
+registration_view,
+UserListView,
+GetUpdateDeleteUser,
+ArticleList,
+ArticleDetailView,
+CreateComment,
+)
 
 # Create your tests here.
+
+
+class TestUrls(SimpleTestCase):
+    print("Url Test")
+
+    def setUp(self):
+        pass
+
+    def test_api_home_is_resolved(self):
+        url = reverse("api_home")
+        self.assertEquals(resolve(url).func, api_home)
+
+    def test_api_list_article_is_resolved(self):
+        url = reverse("article-list")
+        self.assertEquals(resolve(url).func.view_class, ArticleListView)
+
+    def test_api_create_article_is_resolved(self):
+        url = reverse("create-article")
+        self.assertEquals(resolve(url).func, article_create)
+
+    def test_api_update_article_is_resolved(self):
+        url = reverse("update-article",args=[1])
+        self.assertEquals(resolve(url).func.view_class, UpdateArticle)
+
+
+    def test_api_detail_article_is_resolved(self):
+        url = reverse("api-article-detail", args=[1])
+        self.assertEquals(resolve(url).func.view_class, ArticleDetailView)
+
+
+    def test_api_delete_article_is_resolved(self):
+        url = reverse("delete-article",args=[1])
+        self.assertEquals(resolve(url).func, article_delete)
+
+
+    def test_api_view_article_comments_edit_delete_is_resolved(self):
+        url = reverse("edit-delete-comment", args=[1])
+        self.assertEquals(resolve(url).func.view_class, CommentEditDeleteView)
+
+    # def test_api_view_article_comments_create_is_resolved(self):
+    #     url = reverse("create-comment",args=['create'])
+    #     view = CommentViewSet.as_view({'post':"create"})
+    #     self.assertEquals(resolve(url).func, view)
+
+
+    def test_api_register_is_resolved(self):
+        url = reverse("api-register")
+        self.assertEquals(resolve(url).func, registration_view)
+
+    def test_api_users_is_resolved(self):
+        url = reverse("api-list-users")
+        self.assertEquals(resolve(url).func.view_class, UserListView)
+
+    def test_api_view_user_is_resolved(self):
+        url = reverse("api-view-user",args=[1])
+        self.assertEquals(resolve(url).func.view_class, GetUpdateDeleteUser)
+
+    def test_article_detail_is_resolved(self):
+        url = reverse("article-detail", args=[1])
+        self.assertEquals(resolve(url).func.view_class, ArticleDetail)
+
+
+class TestViews(SimpleTestCase):
+    print("View Test")
+    pass
+
+
+class TestModels(TestCase):
+    print("Model Test")
+    pass
+
+
+class TestForms(SimpleTestCase):
+    print("Form Test")
+    pass
+
+
+class TestSerializers(SimpleTestCase):
+    print("Serializer Test")
+    pass
--- blog/urls.py
+++ blog/urls.py
@@ -31,25 +31,24 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     # Article based Api routes
-    path("api/", api_home),
-    path("api/article-list/", ArticleListView.as_view()),
-    path("api/article-create/", article_create),
-    path("api/article-update/<int:pk>", UpdateArticle.as_view()),
-    path("api/article-detail/<int:pk>", ArticleDetailView.as_view()),
-    path("api/article-delete/<int:pk>", article_delete),
-    path("api/article-toggle-draft/<int:pk>", article_toggle_draft),
-    path("api/comment-edit-delete/<int:pk>", CommentEditDeleteView.as_view()),
-    path("api/comment-create/", CommentViewSet.as_view({'post':"create"})),
+    path("api/", api_home, name='api_home'),
+    path("api/article-list/", ArticleListView.as_view(),name='article-list'),
+    path("api/article-create/", article_create,name='create-article'),
+    path("api/article-update/<int:pk>", UpdateArticle.as_view(),name='update-article'),
+    path("api/article-detail/<int:pk>", ArticleDetailView.as_view(),name='api-article-detail'),
+    path("api/article-delete/<int:pk>", article_delete,name="delete-article"),
+    path("api/comment-edit-delete/<int:pk>", CommentEditDeleteView.as_view(),name='edit-delete-comment'),
+    path("api/comment-create/", CommentViewSet.as_view({'post':"create"}),name='create-comment'),
     # path("api/comment-create/", CreateComment.as_view()),
     # User based api routes
-    path("api/register", registration_view),
-    path("api/api-login", obtain_auth_token),
-    path("api/users/", UserListView.as_view()),
-    path("api/users/<int:pk>", GetUpdateDeleteUser.as_view()),
+    path("api/register", registration_view,name='api-register'),
+    path("api/api-login", obtain_auth_token,name='api-login'),
+    path("api/users/", UserListView.as_view(),name='api-list-users'),
+    path("api/users/<int:pk>", GetUpdateDeleteUser.as_view(),name='api-view-user'),
     # comments
     path("comments/", include("fluent_comments.urls")),
     # Template based article routes
     path("", ArticleList.as_view(), name="home"),
-    path("<slug:slug>/", ArticleDetail.as_view(), name="none"),
+    path("<slug:slug>/", ArticleDetail.as_view(), name="article-detail"),
     path("permission-denied", api_permission_denied, name="permission_denied"),
 ]
